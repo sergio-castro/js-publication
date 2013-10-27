66c5d16677b7c265328f774908074e8e886f0e8b
Sergio Castro <sergioc78@gmail.com>
2013-10-27 20:05:25 +0100

    improving on the prettifyCodeSnippets function

--- jquery.sc.tutorial.js
+++ jquery.sc.tutorial.js
@@ -1,30 +1,33 @@
 (function ($) {
 	
 	$.prettifyCodeSnippets = function(theme) {
-				$('*[data-editor]').each(function () {
-	            var original_editor = $(this);
-				 var original_text = original_editor.html();
-	            var mode = original_editor.data('editor');
+		$('*[data-editor]').each(function () {
+			var original_editor = $(this);
+			var original_text = original_editor.html();
+			var mode = original_editor.data('editor');
 
-	            var editDiv = $('<div>', {
-	                position: 'absolute',
-	                width: original_editor.width(),
-	                height: original_editor.height(),
-	                'class': original_editor.attr('class')
-	            }).insertBefore(original_editor);
-	            original_editor.css('visibility', 'hidden');
-
-	            var editor = ace.edit(editDiv[0]);
-	            editor.renderer.setShowGutter(true);
-				 editor.setHighlightActiveLine(false);
-	            editor.getSession().setValue(original_text);
-	            editor.getSession().setMode("ace/mode/" + mode);
-	            editor.setTheme("ace/theme/" + theme);
-				 if('true' != original_editor.attr('contenteditable'))
-				 	editor.setReadOnly(true);
-				 else
-				 	editor.setReadOnly(false);
-	        });
-			}
+			var editDiv = $('<div>', {
+				position: 'absolute',
+				width: original_editor.width(),
+				height: original_editor.height(),
+				'class': original_editor.attr('class')
+				}).insertBefore(original_editor);
+			//original_editor.css('visibility', 'hidden'); //an invisible element still uses space.
+			original_editor.hide(); //equivalent to .css( "display", "none" ), the hidden element does not use space.
+			var editor = ace.edit(editDiv[0]);
+			if(false == original_editor.data('show-gutter'))
+				editor.renderer.setShowGutter(false);
+			else
+				editor.renderer.setShowGutter(true);
+			editor.setHighlightActiveLine(false);
+			editor.getSession().setValue(original_text);
+			editor.getSession().setMode("ace/mode/" + mode);
+			editor.setTheme("ace/theme/" + theme);
+			if('true' != original_editor.attr('contenteditable'))
+				editor.setReadOnly(true);
+			else
+				editor.setReadOnly(false);
+		});
+	}
 	
 })(jQuery);
