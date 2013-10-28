b5d7b1b59d36d21fd7ac87d50514cc21e64fcf1f
Sergio Castro <sergioc78@gmail.com>
2013-10-28 01:16:11 +0100

    added plugin for configuring footnotes

--- jquery.sc.tutorial.js
+++ jquery.sc.tutorial.js
@@ -1,5 +1,8 @@
 (function ($) {
 	
+	/*
+	Adapted from https://gist.github.com/duncansmart/5267653
+	*/
 	$.prettifyCodeSnippets = function(theme) {
 		$('*[data-editor]').each(function () {
 			var original_editor = $(this);
@@ -30,4 +33,93 @@
 		});
 	}
 	
+	
+	/*
+	Core script adapted from http://ignorethecode.net/blog/2010/04/20/footnotes/
+	css style adapted from http://www.leancrew.com/all-this/2010/05/popup-footnotes/
+	*/
+	$.configureFootnotes = function() {
+		var Footnotes = {
+		    footnotetimeout: false,
+		    setup: function() {
+		        var footnotelinks = $("a[rel='footnote']")
+
+		        footnotelinks.unbind('mouseover',Footnotes.footnoteover);
+		        footnotelinks.unbind('mouseout',Footnotes.footnoteoout);
+
+		        footnotelinks.bind('mouseover',Footnotes.footnoteover);
+		        footnotelinks.bind('mouseout',Footnotes.footnoteoout);
+		    },
+		    footnoteover: function() {
+		        clearTimeout(Footnotes.footnotetimeout);
+		        $('#footnotediv').stop();
+		        $('#footnotediv').remove();
+
+		        var id = $(this).attr('href').substr(1);
+		        var position = $(this).offset();
+
+		        var div = $(document.createElement('div'));
+		        div.attr('id','footnotediv');
+		        div.bind('mouseover',Footnotes.divover);
+		        div.bind('mouseout',Footnotes.footnoteoout);
+
+		        var el = document.getElementById(id);
+		        div.html($(el).html());
+		        /*
+		        div.css({
+		            position:'absolute',
+		            width:'400px',
+		            opacity:0.95
+		        });
+		*/
+				div.css({
+			        position:'absolute',
+			/*
+			        width:'400px',
+			*/
+			        background:'#ded',
+			        padding:'0em 1em 0em 1em',
+			        border:'solid 1px',
+			        'font-size':'90%',
+			        'font-family': 'Helvetica, Sans-serif',
+			        'line-height':1.4,
+			        '-moz-border-radius':'.5em',
+			        '-webkit-border-radius':'.5em',
+			        'border-radius':'.5em',
+			        opacity:0.95
+			    });
+
+
+		        $(document.body).append(div);
+
+		        var left = position.left;
+		        if(left + 420  > $(window).width() + $(window).scrollLeft())
+		            left = $(window).width() - 420 + $(window).scrollLeft();
+		        var top = position.top+20;
+		        if(top + div.height() > $(window).height() + $(window).scrollTop())
+		            top = position.top - div.height() - 15;
+		        div.css({
+		            left:left,
+		            top:top
+		        });
+		    },
+		    footnoteoout: function() {
+		        Footnotes.footnotetimeout = setTimeout(function() {
+		            $('#footnotediv').animate({
+		                opacity: 0
+		            }, 600, function() {
+		                $('#footnotediv').remove();
+		            });
+		        },100);
+		    },
+		    divover: function() {
+		        clearTimeout(Footnotes.footnotetimeout);
+		        $('#footnotediv').stop();
+		        $('#footnotediv').css({
+		                opacity: 0.9
+		        });
+		    }
+		}
+		Footnotes.setup();
+	}
 })(jQuery);
